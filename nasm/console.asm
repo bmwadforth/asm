6b58a5159380fd8ec0bc1b346a27cd9f4fa42724
Brannon Wadforth <whiskey@digitalnode.io>
2022-07-24 20:19:40 +1000

    initial commit

---
+++ nasm/console.asm
@@ -0,0 +1,36 @@
+; Prints in the standard output (console) saying hello world
+
+global _main
+extern  _GetStdHandle@4
+extern  _WriteFile@20
+extern  _ExitProcess@4
+
+section .text
+_main:
+    ; DWORD  bytes;    
+    mov     ebp, esp
+    sub     esp, 4
+
+    ; hStdOut = GetstdHandle( STD_OUTPUT_HANDLE)
+    push    -11
+    call    _GetStdHandle@4
+    mov     ebx, eax    
+
+    ; WriteFile( hstdOut, message, length(message), &bytes, 0);
+    push    0
+    lea     eax, [ebp-4]
+    push    eax
+    push    (message_end - message)
+    push    message
+    push    ebx
+    call    _WriteFile@20
+
+    ; ExitProcess(0)
+    push    0
+    call    _ExitProcess@4
+
+    ; never here
+    hlt
+message:
+    db      'Hello, World', 10
+message_end:
---
+++ nasm/main.asm
@@ -0,0 +1,20 @@
+; Opens a message box saying hello world
+
+global _main
+extern _MessageBoxA@16
+extern _ExitProcess@4
+
+section .text use32 class=code
+_main:
+	push	dword 0      ; UINT uType = MB_OK
+	push	dword title  ; LPCSTR lpCaption
+	push	dword banner ; LPCSTR lpText
+	push	dword 0      ; HWND hWnd = NULL
+	call	_MessageBoxA@16
+
+	push	dword 0      ; UINT uExitCode
+	call	_ExitProcess@4
+
+section data use32 class=data
+	banner:	db 'Hello, world!', 0
+	title:	db 'Hello', 0
