c1668fdab15c9f3238ffa795624d5829e6fb64eb
Brannon Wadforth <whiskey@digitalnode.io>
2022-08-01 22:30:03 +1000

    Updating conditionals logic

--- nasm/src/03_Conditionals/Conditionals.asm
+++ nasm/src/03_Conditionals/Conditionals.asm
@@ -15,10 +15,29 @@ Unconditional_Jumps:
     mov rdx, body
     call Windows_x64_PrintDialog
 
+; rcx is a special register when used with loop. This instruction will loop until rcx is 0, so 8 times.
+; We know this is true because if we were to return here, when C++ gets the return value from this
+; routine it will be 8 (as rax is the return register).
+    mov rax, 0
+    mov rcx, 8
+.loop8Times:
+    inc rax
+    loop .loop8Times
+
+; here is the equivalent code as above but without the cool functionality of loop and rcx
+    mov rax, 0
+    mov rcx, 8
+.loop8TimesWithoutRCX:
+    inc rax
+    dec rcx
+    cmp rcx, 0
+    jnz .loop8TimesWithoutRCX
+
+; unconditional jump to label
     jmp .JumpLabel       ; jumping to jump label
 
 .JumpLabel:
-    mov rax, 10         ; moving 10 into rax (return value) so Unconditional_Jumps() will return 10.
+    ;mov rax, 10         ; moving 10 into rax (return value) so Unconditional_Jumps() will return 10.
 
     ; function epilogue
     add    rsp, 28h       ; remove 40 bytes of shadow space, aligns stack
