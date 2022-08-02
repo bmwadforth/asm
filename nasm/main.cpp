6b2f0797004444b2b7a9986d0bc4fb9af6ca0ff3
Brannon Wadforth <whiskey@digitalnode.io>
2022-08-02 20:55:49 +1000

    adding loop logic

--- nasm/main.cpp
+++ nasm/main.cpp
@@ -4,11 +4,12 @@ extern "C" {
 void x64_00_SystemCalls_Dialog(char *title, char *body);
 int x64_03_Conditionals();
 int x64_04_Procedures_Sum(int a, int b, int c, int d, int e);
-void x64_05_Loops();
+int x64_05_Loops(int);
 }
 
 int main() {
     // 00_SystemCalls
+    /*
     char *title = "00_SystemCalls";
     char *body = "Hello from C++!";
     x64_00_SystemCalls_Dialog(title, body);
@@ -21,8 +22,11 @@ int main() {
     auto sum = x64_04_Procedures_Sum(1, 2, 3, 4, 5);
     std::cout << "04_Procedures: SumProcedure returned: " << sum << std::endl;
 
+     */
+
     // 05_Loops
-    x64_05_Loops();
+    int looped = x64_05_Loops(5);
+    std::cout << "05_Loops: x64_05_Loops looped: " << looped << " times" << std::endl;
 
     return 0;
 }
--- nasm/src/05_Loops/Loops.asm
+++ nasm/src/05_Loops/Loops.asm
@@ -1,27 +1,46 @@
-extern Windows_x64_PrintDialog
+extern x64_00_SystemCalls_Dialog
 global x64_05_Loops
 
 section .data
+    title db '05_Loops', 0
+    body db 'Loop example', 0
 
 section .text
+;int x64_05_Loops(int amountOfTimesToLoop);
 x64_05_Loops:
-; rcx is a special register when used with loop. This instruction will loop until rcx is 0, so 8 times.
-; We know this is true because if we were to return here, when C++ gets the return value from this
-; routine it will be 8 (as rax is the return register).
-    mov rax, 0
-    mov rcx, 8
+    ; rcx is a special register when used with loop. This instruction will loop until rcx is 0, so the below code would loop 8 times.
+    ; and also increment rax 8 times. Code won't need to define mov rcx, 8 if rcx is being set by the function that makes the call. (fast call argument passing).
+
+    ;    mov rax, 0
+    ;    mov rcx, 8
+    ;  .loop8Times:
+    ;    inc rax
+    ;    loop .loop8Times
+
+
+
+
+    ; below we will define some logic that doesn't use the fancy rcx counter, as we are calling a function each iteration, which could break our counter value (as RCX is a volatile register)
+
+    mov DWORD [rsp - 8], 0      ; we want to use a local variable on the stack. We know the variable is an integer so we can use DWORD.
+                                ; We are telling the assembler here that whatever lives at RSP - 8 (in this case an int) is a double word (32 bits)
+
+    mov QWORD [rsp - 16], rcx   ; We will use this as our incrementer as rcx is volatile between function calls. Also have to define QWORD as rcx is 64bit and QWORD == 64 bits
+
 .loop8Times:
-    inc rax
-    loop .loop8Times
+    inc DWORD [rsp - 8]         ; increment our first local variable
+    dec DWORD [rsp - 16]        ; decrement our second local variable (what rcx initially was)
 
-; here is the equivalent code as above but without the cool functionality of loop and rcx
-    mov rax, 0
-    mov rcx, 8
-.loop8TimesWithoutRCX:
-    inc rax
-    dec rcx
-    cmp rcx, 0
-    jnz .loop8TimesWithoutRCX
+    mov rcx, title
+    mov rdx, body
+    sub rsp, 0x28
+    call x64_00_SystemCalls_Dialog
+    add rsp, 0x28
 
 
+    cmp DWORD [rsp - 16], 0     ; check if our second local variable is 0, if it is, set a flag in the eflags register
+    jnz .loop8Times             ; if not zero, jump back to start of label and go again
+
+
+    mov rax, [rsp - 8]
     ret
